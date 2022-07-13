d3473f552c2fec09f53986ddc1a4aa47fb6c5c50
Jh Do <dojh99@gmail.com>
2022-07-13 09:20:13 +0900

    fix: remove multiprocess mode & add using async

--- usersearcher.py
+++ usersearcher.py
@@ -3,7 +3,8 @@ import requests
 import sys
 from functools import partial
 from multiprocessing import Pool
-
+import asyncio
+import time
 
 # 'service name' : "link"
 services = {
@@ -49,7 +50,10 @@ services = {
     'weibo': "https://www.weibo.com/{username}"  # not 404
 }
 
-def username_exist_in_service(service,username,link=None):
+def get(link,allow_redirects = True):
+    return requests.get(link,allow_redirects=allow_redirects)
+
+async def username_exist_in_service(service,username,link=None):
 
     _link = None
     if link is not None:
@@ -58,10 +62,12 @@ def username_exist_in_service(service,username,link=None):
         _link = services[service]
         _link = _link.format(username = username)
 
+    loop = asyncio.get_running_loop()
+
     # services should be determined by using tricks
     # services which redirect to other page when there's no userinfo
     if service in ['wordpress','weibo']:
-        response = requests.get(_link,allow_redirects = False)
+        response = await loop.run_in_executor(None,get,_link, False)
         return response.status_code == 200
 
     elif service == 'steam':
@@ -78,49 +84,27 @@ def username_exist_in_service(service,username,link=None):
 
     # service can be determined by http status code
     else :
-        response = requests.get(_link)
+        response = await loop.run_in_executor(None,get,_link)
         return response.status_code == 200
         
 
-def username_exist_in_services(username,use_multiprocess=False,service_names = services.keys(),include_none=False):
+async def username_exist_in_services(username,service_names = services.keys(),include_none=False):
     result = {}
     if not len(username):
         print("username length is 0")
         return None
         
-    if use_multiprocess:
-        uei = partial(username_exist_in_service,username = username)
+    uei = partial(username_exist_in_service,username = username)
 
-        pol = Pool(len(service_names))
-        exists = pol.map(uei, service_names)
-        
-        for i, service in enumerate(service_names):
-            if exists[i]:
-                result[service] = services[service].format(username = username)
-            else:
-                if include_none :
-                    result[service] = None
-
-    else:
-        for service, link in list(filter(lambda x:x[0] in service_names , services.items())):
-
-            exist = False
-
-            try:
-                exist = username_exist_in_service(service,username,link)
-
-            except KeyboardInterrupt:
-                print("Keyboard Interrupt detected")
-                sys.exit()
-            except:
-                print(service, " Error occured!")
-                continue
-
-            if exist:
-                result[service]=link.format(username = username)
-            else:
-                if include_none :
-                    result[service]=None
+    exists_future = [asyncio.ensure_future(uei(service_name)) for service_name in service_names]
+    exists = await asyncio.gather(*exists_future)
+    
+    for i, service in enumerate(service_names):
+        if exists[i]:
+            result[service] = services[service].format(username = username)
+        else:
+            if include_none :
+                result[service] = None
 
     return result
 
@@ -128,15 +112,19 @@ def username_exist_in_services(username,use_multiprocess=False,service_names = s
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='UserSearcher')
     parser.add_argument('username',help='Username to search.')
-    parser.add_argument('--multiprocess','-m',type=bool,default=True,help='Option to use multiprocess method. Default value is True')
     args = parser.parse_args()
 
     username = args.username
-    use_multiprocess = args.multiprocess
 
-    result = username_exist_in_services(username,use_multiprocess=use_multiprocess)
+    t1 = time.time()
+    loop = asyncio.new_event_loop()
+    result = loop.run_until_complete(username_exist_in_services(username))
+    loop.close()
 
     print("Search result with username ",username)
     for service, url in result.items():
         print(service, " : ",url)
+
+    t2 = time.time()
+    print("Done in : " ,t2-t1)
     
